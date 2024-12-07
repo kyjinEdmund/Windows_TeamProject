c430f7dc6fe2777d27aaa956f3b7c92bd7e8b3cd
KimYongjin <pctlall1223@naver.com>
2024-12-07 15:25:25 +0900

    Node Class구현

---
+++ Generating_flowchart/Generating_flowchart/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_flowchart
+{
+    public class Node
+    {
+        public string Shape { get; set; }
+        public string Text { get; set; }
+        public int NodeID { get; set; }
+        public List<Node> Cases { get; set; }
+        public ParsedComponent FromComp { get; set; }
+        public int NodeLevel { get; set; }
+
+        // 생성자
+        public Node(string shape, string text, int nodeId, ParsedComponent from_comp)
+        {
+            Shape = shape;
+            Text = text;
+            NodeID = nodeId;
+            Cases = new List<Node>();
+            FromComp = from_comp;
+        }
+        public void AddCases(Node node)
+        {
+            Cases.Add(node);
+        }
+        public string ToString()
+        {
+            return $"{NodeID} {Shape} {Text}";
+        }
+    }
+}

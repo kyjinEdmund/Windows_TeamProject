c19446f267be802a67c23414a3bd424a2d02ae64
KimYongjin <pctlall1223@naver.com>
2024-12-07 14:50:37 +0900

    RoundedRactangle 컴포넌트 구현

--- Diagram/RoundedRactangle/RoundedRactangle.Designer.cs
+++ Diagram/RoundedRactangle/RoundedRactangle.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RoundedRactangle
 {
-    partial class UserControl1
+    partial class RoundedRactangle
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion

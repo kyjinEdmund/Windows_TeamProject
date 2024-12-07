9f835e7e5fde2189404dea69f208872cdf23cca7
KimYongjin <pctlall1223@naver.com>
2024-12-07 14:57:14 +0900

    직사각형 컴포넌트 구현

--- Diagram/Ractangle/Ractangle.Designer.cs
+++ Diagram/Ractangle/Ractangle.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ractangle
 {
-    partial class UserControl1
+    partial class Ractangle
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
--- Diagram/Ractangle/Ractangle.cs
+++ Diagram/Ractangle/Ractangle.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Ractangle
 {
-    public partial class UserControl1: UserControl
+    public partial class Ractangle: UserControl
     {
-        public UserControl1()
+        public Ractangle()
         {
             InitializeComponent();
         }

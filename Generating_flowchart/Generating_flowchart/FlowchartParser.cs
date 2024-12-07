4ca3d56e2777deeb19c954bde5b36d614ca2dbce
KimYongjin <pctlall1223@naver.com>
2024-12-07 15:22:33 +0900

    ParsedComponent Class 선언 및 정의
    enum CodeType 정의

--- Generating_flowchart/Generating_flowchart/FlowchartParser.cs
+++ Generating_flowchart/Generating_flowchart/FlowchartParser.cs
@@ -9,4 +9,62 @@ namespace Generating_flowchart
     internal class FlowchartParser
     {
     }
+
+    public enum CodeType
+    {
+        If, // diamond
+        ElseIf,  // diamond
+        Else,  // 노드 없음
+        VariableAssignment,  // Rectangle
+        VariableDeclaration,  // Rectangle
+        FunctionDeclaration,// Oval
+        FunctionReturn,// Oval
+        Continue,// 노드 없음
+        Break,// 노드 없음
+        BlockOperator,  // 노드 없음
+        BlockOperatorOpen,  // 노드 없음
+        BlockOperatorClose, // 노드 없음
+        While,  // 
+        For,
+        Printf,
+        Scanf
+    }
+
+
+    public class ParsedComponent
+    {
+        private CodeType Type { get; set; }
+
+        private string Text { get; set; }
+        private List<ParsedComponent> Children { get; set; }
+
+
+        public ParsedComponent(CodeType type, string text)
+        {
+            Type = type;
+            Text = text;
+            Children = new List<ParsedComponent>();
+
+        }
+        public void AddChildren(ParsedComponent child)
+        {
+            Children.Add(child);
+        }
+        public CodeType GetCodeType() { return Type; }
+        public string GetText() { return Text; }
+        public List<ParsedComponent> GetChildren() { return Children; }
+        public void PrintSelf(string beforeText = "")  // 생각처럼 출력이 안됨
+        {
+            Console.WriteLine($"{beforeText}{Type}, {Text}");
+            if (Type == CodeType.BlockOperator)
+            {
+                foreach (ParsedComponent child in Children)
+                {
+                    child.PrintSelf($"{beforeText}    ");
+                }
+            }
+
+        }
+
+    }
 }
